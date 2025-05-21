b6fb72d07cc9e772c1f219f81129d98bfc544615
Emily Kanarek <emilyakanarek@gmail.com>
2025-05-21 08:45:18 -0400

    Check if word is a palindrome

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const wordBackwards = word.split("").toReversed().join("");
+  return word === wordBackwards ? true : false;
 }
 
-/* 
-  Add your pseudocode here
+/*
+Pseudocode:
+Takes string as an argument
+Iterates over the string and creates a new string of the same letters, backwards
+Compares argument to the new string
+Returns true if word === backwards word
+Returns false if word !== backwards word
 */
 
 /*
-  Add written explanation of your solution here
+Code Explanation:  
+- the wordBackwards variable is set to the argument split into an array, reversed, and then re-joined into a string
+  - argument is compared to wordBackwards, returns true if they are deeply equal and false if they are not
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
+  console.log("Expecting: true");
+  console.log(isPalindrome("tacocat"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log(isPalindrome("palindrome"));
+
+  console.log("");
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
